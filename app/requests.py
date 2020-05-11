e818da14924783d72330d3bfcdc7c0801f48b0cf
lorrainekamanda <lorrainekamanda@gmail.com>
2020-05-11 11:38:40 +0300

    add urrlib request

--- app/requests.py
+++ app/requests.py
@@ -1,7 +1,11 @@
 from app import app
-import urllib.request,json
-with urlopen('http://quotes.stormconsultancy.co.uk/random.json') as response:
-    source = response.read()
-
-print(source)
-
+import urllib.request as request
+import json 
+def random_quotes():
+    with request.urlopen('http://quotes.stormconsultancy.co.uk/random.json') as response:
+        if response.getcode() == 200:
+            source = response.read()
+            data = json.loads(source)
+        else:
+            print('An error occurred while attempting to retrieve data from the API.')
+    return data
